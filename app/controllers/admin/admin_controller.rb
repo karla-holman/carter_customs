552fecbd13810cf12a50b6d46ddb5de0c4d84162
Karla Holman <holmankarla@gmail.com>
2016-06-12 20:52:55 -0700

    Added another slide, fixed copy

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,7 +3,11 @@ module Admin
 		before_filter :validate_user
 
 		def validate_user
-			true
+			if spree_current_user && spree_current_user.admin?
+				true
+			else
+				redirect_to root_path, :error => "Must be logged in as admin to access that page"
+			end
 		end
 	end
 end
