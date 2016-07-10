bf5b8aae9e2e85adce5cc6141faff581eb83251d
Karla Holman <holmankarla@gmail.com>
2016-07-09 22:18:04 -0700

    Fix AWS region

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => 'us-west-2',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => 'us-west-2',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'us-west-2'
     }
   }
 end
